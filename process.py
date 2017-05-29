a8bf279f68bad770dce46f7410ac9e9c2ae05fb3
s.matthew.english <s.matthew.english@gmail.com>
2017-05-29 22:25:06 +0200

    add support for identical files

--- process.py
+++ process.py
@@ -1,16 +1,18 @@
 # this script is meant to operate on the output of the following command:
 # 
-# diff --brief -r go-ethereum/ quorum/ >> diff_result.txt
+# diff --report-identical-files --brief -r go-ethereum/ quorum/ >> diff_id_result.txt
 # 
 import re
 
-with open("diff_result.txt", "r") as input_file:
+with open("diff_id_result.txt", "r") as input_file:
     geth_files   = []
     geth_substring = "Only in go-ethereum/"
     quorum_files = []
     quorum_substring = "Only in quorum/"
     files_differ = []
-    files_substring = "Files go-ethereum/"
+    differ_substring = "differ"
+    files_identical = []
+    identical_substring = "identical"
 
     for line in input_file:
         if geth_substring in line:
@@ -19,11 +21,14 @@ with open("diff_result.txt", "r") as input_file:
         if quorum_substring in line:
             unique_to_quorum = line[line.index(quorum_substring) + len(quorum_substring):]
             quorum_files.append(unique_to_quorum.rstrip('\n'))
-        if files_substring in line:
-            # find the files that are the same in both
-            regex = re.compile('{}(.*){}'.format(re.escape('Files go-ethereum/'), re.escape(' and')))
+        if differ_substring in line:
+            regex = re.compile('{}(.*){}'.format(re.escape('and quorum/'), re.escape(' differ')))
             string_output = regex.findall(line)
             files_differ.append( string_output[0] )
+        if identical_substring in line:
+            regex = re.compile('{}(.*){}'.format(re.escape('and quorum/'), re.escape(' are identical')))
+            string_output = regex.findall(line)
+            files_identical.append( string_output[0] )
 
 print('\n\nONLY IN QUORUM:\n')
 for quo_element in quorum_files: 
@@ -31,9 +36,12 @@ for quo_element in quorum_files:
 print('\n\nONLY IN GO-ETHEREUM:\n')
 for geth_element in geth_files: 
     print( geth_element )
-print('\n\nFILES DIFFER:\n')
+print('\n\nFILES IN BOTH, BUT DIFFERENT:\n')
 for diff_element in files_differ: 
     print( diff_element )
+print('\n\nFILES ARE IDENTICAL:\n')
+for id_element in files_identical: 
+    print( id_element )
 
 
 
